90c35baa75c4e63366d44f370913528cbe586e29
Jake Lazaroff <yo@jake.nyc>
2015-12-08 00:39:13 -0500

    Add plugin

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 var postcss = require('postcss');
 
-module.exports = postcss.plugin('PLUGIN_NAME', function (opts) {
-    opts = opts || {};
+function replaceDeclaration(props) {
+    return function (declaration) {
+        props.forEach(function (prop) {
+            declaration.cloneBefore({ prop: prop });
+        });
 
-    // Work with options here
+        declaration.remove();
+    };
+}
 
-    return function (css, result) {
+module.exports = postcss.plugin('postcss-border-radius', function () {
+    return function (css) {
 
-        // Transform CSS AST here
+        css.walkDecls('border-top-radius', replaceDeclaration([
+            'border-top-right-radius',
+            'border-top-left-radius'
+        ]));
 
+        css.walkDecls('border-right-radius', replaceDeclaration([
+            'border-top-right-radius',
+            'border-bottom-right-radius'
+        ]));
+
+        css.walkDecls('border-bottom-radius', replaceDeclaration([
+            'border-bottom-right-radius',
+            'border-bottom-left-radius'
+        ]));
+
+        css.walkDecls('border-left-radius', replaceDeclaration([
+            'border-top-left-radius',
+            'border-bottom-left-radius'
+        ]));
     };
 });
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import postcss from 'postcss';
-import test    from 'ava';
+import test from 'ava';
 
 import plugin from './';
 
@@ -11,10 +11,38 @@ function run(t, input, output, opts = { }) {
         });
 }
 
-/* Write tests here
-
-test('does something', t => {
-    return run(t, 'a{ }', 'a{ }', { });
+test('replaces border-top-radius', t => {
+    return run(t,
+        'a{ border-top-radius: 5px; }',
+        'a{ border-top-right-radius: 5px; border-top-left-radius: 5px; }',
+        { });
 });
 
-*/
+test('replaces border-right-radius', t => {
+    return run(t,
+        'a{ border-right-radius: 5px; }',
+        'a{ border-top-right-radius: 5px; border-bottom-right-radius: 5px; }',
+        { });
+});
+
+test('replaces border-bottom-radius', t => {
+    return run(t,
+        'a{ border-bottom-radius: 5px; }',
+        'a{ border-bottom-right-radius: 5px; border-bottom-left-radius: 5px; }',
+        { });
+});
+
+test('replaces border-left-radius', t => {
+    return run(t,
+        'a{ border-left-radius: 5px; }',
+        'a{ border-top-left-radius: 5px; border-bottom-left-radius: 5px; }',
+        { });
+});
+
+test('works with multiple values', t => {
+    return run(t,
+        'a{ border-top-radius: 5px 10px; }',
+        'a{ border-top-right-radius: 5px 10px; ' +
+        'border-top-left-radius: 5px 10px; }',
+        { });
+});
